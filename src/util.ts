dc185657de72cd6804e839022d16662a75ccf1c0
Technote <technote.space@gmail.com>
2020-06-28 01:40:05 +0900

    feat: draft behavior

--- src/util.ts
+++ src/util.ts
@@ -1,15 +1,16 @@
 import * as glob from 'glob';
-import {lstatSync} from 'fs';
+import {lstatSync, readFileSync} from 'fs';
 
 export interface Config {
   'github_ref': string;
-  'github_repository': string;
   // user provided
   'input_name'?: string;
   'input_body'?: string;
   'input_body_path'?: string;
   'input_files'?: string[];
-  'input_draft'?: boolean;
+  'input_create_draft_mode'?: boolean;
+  'input_update_draft_flag'?: boolean;
+  'input_update_draft_mode'?: boolean;
   'input_prerelease'?: boolean;
 }
 
@@ -29,14 +30,23 @@ export const parseInputFiles = (files: string): string[] => {
 export const parseConfig = (env: Env): Config => {
   return {
     'github_ref': env.GITHUB_REF || '',
-    'github_repository': env.GITHUB_REPOSITORY || '',
     'input_name': env.INPUT_NAME,
     'input_body': env.INPUT_BODY,
     'input_body_path': env.INPUT_BODY_PATH,
     'input_files': parseInputFiles(env.INPUT_FILES || ''),
-    'input_draft': env.INPUT_DRAFT === 'true',
+    'input_create_draft_mode': env.INPUT_CREATE_DRAFT_MODE === 'true',
+    'input_update_draft_flag': env.INPUT_UPDATE_DRAFT_FLAG === 'true',
+    'input_update_draft_mode': env.INPUT_UPDATE_DRAFT_MODE === 'true',
     'input_prerelease': env.INPUT_PRERELEASE === 'true',
   };
+};
+
+export const releaseBody = (config: Config): string | undefined => {
+  return (
+    config.input_body ||
+    (config.input_body_path &&
+      readFileSync(config.input_body_path).toString('utf8'))
+  );
 };
 
 export const paths = (patterns: string[]): string[] => {
